19ac617d4c05bde2b85d50e030296ce471ba12b9
scv829 <scv829@naver.com>
2024-08-12 15:00:33 +0900

    Feat: 메인 화면 추가
    게임 접속 시 처음 보이는 화면 추가

---
+++ Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryFarm.Scenes;
+
+namespace GeometryFarm
+{
+    public class Game
+    {
+        private bool isRunning;
+        private Scene[] scenes;
+        private Scene currntScene;
+
+        public void Run()
+        {
+            Start();
+            while (isRunning)
+            {
+                Render();
+                Input();
+                Update();
+            }
+            End();
+        }
+
+        public void Over()
+        {
+            isRunning = false;
+        }
+
+        private void Start()
+        {
+            isRunning = true;
+
+            scenes = new Scene[(int)SceneType.SIZE];
+            
+            scenes[(int)SceneType.Main] = new MainScene(this);
+            
+            currntScene = scenes[(int)SceneType.Main];
+
+            currntScene.Enter();
+        }
+
+        private void Render()
+        {
+            currntScene.Render();
+        }
+
+        private void Input()
+        {
+            currntScene.Input();
+        }
+
+        private void Update()
+        {
+            currntScene.Update();
+        }
+
+        private void End()
+        {
+            currntScene.Exit();
+        }
+
+    }
+}
---
+++ Scenes/MainScene.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryFarm.Scenes
+{
+    public class MainScene : Scene
+    {
+
+        private string input;
+
+        public MainScene(Game game) : base(game)
+        {
+        }
+
+        public override void Enter()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+        }
+
+        public override void Input()
+        {
+            // 키 입력 받기
+            input = Console.ReadLine();
+            
+        }
+
+        public override void Render()
+        {
+            // 타이틀 보여주기
+            Console.WriteLine("도형 농장");
+            Console.WriteLine("1. 게임 시작");
+            Console.WriteLine("2. 게임 종료");
+            // 1. 게임 시작
+            // 2. 게임 종료
+        }
+
+        public override void Update()
+        {
+            // 키 입력 받은걸 기준으로 맵 이동
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("게임 시작");
+                    break;
+                case "2":
+                    Console.WriteLine("게임 종료");
+                    game.Over();
+                    break;
+                default:
+                    Console.WriteLine("입력 오류");
+                    break;
+            }
+            
+        }
+    }
+}
