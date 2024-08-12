7f94076c6757321e4362f41cf5c86fc7d9059173
scv829 <scv829@naver.com>
2024-08-12 16:44:48 +0900

    Feat: 플레이어 추가
    기본적인 플레이어 추가

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using GeometryFarm.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryFarm
+{
+    public class Player
+    {
+        private Item[] inventory;
+        public int gold { get; private set; }
+        public int fatigue { get; private set; }
+        private string name;
+        private int inventoryIndex;
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.inventory = new Item[6];
+            this.gold = 3000;
+            this.fatigue = 0;
+            this.inventoryIndex = 0;
+        }
+
+        public bool isInventoryFull()
+        {
+            return inventoryIndex >= inventory.Length;
+        }
+
+        public void BuyItem(Item item)
+        {
+            inventory[inventoryIndex++] = item;
+            gold -= item.price;
+        }
+
+    }
+}
