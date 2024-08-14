599c61e4912f2136bcc1af232b82db8bc4c1400a
scv829 <scv829@naver.com>
2024-08-14 17:46:30 +0900

    Feat: Crop, Seed 팩토리 추가
    Crop, Seed 팩토리 추가

---
+++ Items/CropFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryFarm.Items
+{
+    public class CropFactory
+    {
+        public static Crop Instantiate(string name)
+        {
+            if(name == "네모")
+            {
+                Crop crop = new Crop("네모 농작물", 150, "네모 농작물");
+                crop.Shape = 'u';
+                return crop;
+            }
+            else if (name == "꽉찬 네모")
+            {
+                Crop crop = new Crop("꽉찬 네모 농작물", 300, "속이 꽉찬 네모 농작물");
+                crop.Shape = 'U';
+                return crop;
+            }
+            else if (name == "세모")
+            {
+                Crop crop = new Crop("세모 농작물", 100, "세모 농작물");
+                crop.Shape = 'v';
+                return crop;
+            }
+            else if (name == "꽉찬 세모")
+            {
+                Crop crop = new Crop("꽉찬 세모 농작물", 200, "속이 꽉찬 세모 농작물");
+                crop.Shape = 'V';
+                return crop;
+            }
+            else if (name == "원형")
+            {
+                Crop crop = new Crop("원형 농작물", 50, "원형 농작물");
+                crop.Shape = 'o';
+                return crop;
+            }
+            else if (name == "꽉찬 원형")
+            {
+                Crop crop = new Crop("꽉찬 원형 농작물", 100, "속이 꽉찬 원형 농작물");
+                crop.Shape = 'O';
+                return crop;
+            }
+            else
+            {
+                Console.WriteLine("해당 이름의 아이템이 없습니다.");
+                return null;
+            }
+        }
+    }
+}
---
+++ Items/SeedFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryFarm.Items
+{
+    public class SeedFactory
+    {
+        public static Seed Instantiate(string name)
+        {
+            Random random = new Random();
+            double rand = random.NextDouble();
+            if (name == "네모")
+            {
+                Seed seed = new Seed("네모 씨앗", 50, "네모 농산물의 씨앗입니다.", 4);
+                seed.Parent = (0.9 < rand) ? CropFactory.Instantiate("꽉찬 네모") : CropFactory.Instantiate("네모");
+                return seed;
+            }
+            else if (name == "세모")
+            {
+                Seed seed = new Seed("세모 씨앗", 30, "세모 농산물의 씨앗입니다.", 3);
+                seed.Parent = (0.9 < rand) ? CropFactory.Instantiate("꽉찬 세모") : CropFactory.Instantiate("세모");
+                return seed;
+            }
+            else if (name == "원형")
+            {
+                Seed seed = new Seed("원형 씨앗", 30, "원형 농산물의 씨앗입니다.", 2);
+                seed.Parent = (0.9 < rand) ? CropFactory.Instantiate("꽉찬 원형") : CropFactory.Instantiate("원형");
+                return seed;
+            }
+            else
+            {
+                Console.WriteLine("해당 이름의 아이템이 없습니다.");
+                return null;
+            }
+        }
+    }
+}
